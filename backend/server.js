9f5dd218577af04e43164fd13bdbbc14220eefe3
salehamunshi <saleha.munshi@ada.ac.uk>
2025-03-22 17:28:34 +0000

    be: connect to database

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send("Budget Tracker API is running...");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
